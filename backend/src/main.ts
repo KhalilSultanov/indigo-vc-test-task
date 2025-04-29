288ec799c20ecac37402f9bde0df4449016ce4a1
Khalil <h.sultanoff@yandex.ru>
2025-04-29 14:31:56 +0300

    update cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: ['http://localhost:80', 'http://127.0.0.1'],
+        origin: ['http://localhost', 'http://localhost:80'],
         credentials: true,
     });
 
